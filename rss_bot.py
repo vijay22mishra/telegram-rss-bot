775536a8726fd993e00e4b284acf5c4257f8b36a
vijay22mishra <73885362+vijay22mishra@users.noreply.github.com>
2026-02-19 22:51:05 +0530

    Create rss_bot.py

---
+++ rss_bot.py
@@ -0,0 +1,21 @@
+import requests
+import feedparser
+
+BOT_TOKEN = "8563577508:AAGvTSX2NunzroN0vjbvtZ69n6fQjkL5EO4"
+CHAT_ID = "-1003502019216"
+RSS_URL = "https://feeds.reuters.com/reuters/businessNews"
+
+feed = feedparser.parse(RSS_URL)
+
+for entry in feed.entries[:5]:
+    title = entry.title
+    link = entry.link
+    message = f"{title}\n{link}"
+    
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+    
+    requests.post(url, data=payload)
