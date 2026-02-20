7f99b64f751356cff3af804d8e04566265a29e66
vijay22mishra <73885362+vijay22mishra@users.noreply.github.com>
2026-02-20 07:09:07 +0530

    Update rss_bot.py

--- rss_bot.py
+++ rss_bot.py
@@ -1,21 +1,53 @@
 import requests
 import feedparser
+import json
+import os
 
 BOT_TOKEN = "8563577508:AAGvTSX2NunzroN0vjbvtZ69n6fQjkL5EO4"
 CHAT_ID = "-1003502019216"
-RSS_URL = "https://khabargaon.com/category/business/feed"
 
-feed = feedparser.parse(RSS_URL)
+RSS_FEEDS = [
+    "https://khabargaon.com/category/lifestyle/feed",
+    "https://khabargaon.com/category/business/feed",
+    "https://khabargaon.com/category/sports/feed",
+    "https://khabargaon.com/category/entertainment/feed",
+    "https://khabargaon.com/category/religion/feed",
+    "https://khabargaon.com/category/state/feed",
+    "https://khabargaon.com/category/international/feed",
+    "https://khabargaon.com/category/national/feed"
 
-for entry in feed.entries[:5]:
-    title = entry.title
-    link = entry.link
-    message = f"{title}\n{link}"
-    
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": message
-    }
-    
-    requests.post(url, data=payload)
+]
+
+SEEN_FILE = "seen_links.json"
+
+# Load old links
+if os.path.exists(SEEN_FILE):
+    with open(SEEN_FILE, "r") as f:
+        seen_links = set(json.load(f))
+else:
+    seen_links = set()
+
+new_seen = set(seen_links)
+
+for feed_url in RSS_FEEDS:
+    feed = feedparser.parse(feed_url)
+
+    for entry in feed.entries[:10]:
+        link = entry.link
+
+        if link in seen_links:
+            continue
+
+        title = entry.title
+        message = f"{title}\n{link}"
+
+        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+        payload = {"chat_id": CHAT_ID, "text": message}
+
+        requests.post(url, data=payload)
+
+        new_seen.add(link)
+
+# Save updated links
+with open(SEEN_FILE, "w") as f:
+    json.dump(list(new_seen), f)
